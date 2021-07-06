105e862b74d70a7a1d09a3cc15486f7beae189c5
mahardikakrisna <mahardika.krisna@intern.bukalapak.com>
2021-07-06 10:57:34 +0700

    Inital commit

--- main.go
+++ main.go
@@ -4,14 +4,59 @@ import (
 	"fmt"
 	"net/http"
 )
+func add(num_a, num_b int) (output int) {
+	output = num_a + num_b
+	return 
+} 
+
+func sub(num_a, num_b int) (output int) {
+	output = num_a - num_b
+	return 
+} 
+
+func mul(num_a, num_b int) (output int) {
+	output = num_a * num_b
+	return 
+} 
+
+func div(num_a, num_b int) (output int) {
+	output = num_a / num_b
+	return 
+} 
+
+func pow(base, exp int) (output int) {
+	output = base ** exp
+	return 
+} 
 
 func calculatorHandler(w http.ResponseWriter, req *http.Request) {
 
 	numA := req.URL.Query().Get("num_a")
 	numB := req.URL.Query().Get("num_b")
 	operator := req.URL.Query().Get("operator")
-
-	fmt.Fprintf(w, fmt.Sprint(numA, numB, operator))
+	res := 0
+	switch operator {
+		case "+":
+			res = add(numA, numB)
+			break
+		case "-":
+			res = sub(numA, numB)
+			break
+		case "*":
+			res = mul(numA, numB)
+			break
+		case "/":
+			res = div(numA, numB)
+			break
+		case "^":
+			res = pow(numA, numB)
+			break
+		case "**":
+			res = pow(numA, numB)
+			break
+		
+	}
+	fmt.Fprintf(w, fmt.Sprint(res))
 }
 
 func main() {
